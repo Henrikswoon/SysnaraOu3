77f24ad56ba76402cd93ac35142412bfab8b656c
Melker Henriksson <melhen12344@gmail.com>
2024-10-20 21:31:48 +0200

    Added timestamp to .csv

--- timed_run.sh
+++ timed_run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
-out_f="_performance_data.csv"
+timestamp=$(date +%H%M%S)
+out_f="performance_data_$timestamp.csv"
 echo "threads,time_seconds" > "$out_f"
-for i in $(seq 1 10); do
-    for j in $(seq 1 100); do
-        echo "iteration: $i, threadcount: $j"
-        out=$({ time ./mdu -j "$j" /pkg; } 2>&1)  # Ta bort mellanslaget mellan 'out' och '='
-        elapsed_time=$(echo "$out" | grep real | awk '{print $2}' | sed 's/[^0-9.]*//g')
-        echo "$j,$elapsed_time" >> "$i$out_f"
-    done
+for j in $(seq 1 100); do
+    echo "threadcount: $j"
+    out=$({ time ./mdu -j "$j" /pkg; } 2>&1)  # Ta bort mellanslaget mellan 'out' och '='
+    elapsed_time=$(echo "$out" | grep real | awk '{print $2}' | sed 's/[^0-9.]*//g')
+    echo "$j,$elapsed_time" >> "$i$out_f"
 done
 
 echo "Results stored to $out_f."
